52e29d8bcb5edf1164e8bceffc4b1b7c0b7d6061
Viktoria <zaharova181114@gmail.com>
2022-04-27 11:48:51 +0300

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,39 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$(function () {
+    $('.twenty-images').twentytwenty({
+        before_label: 'До',
+        after_label: 'После'
+    });
 });
+
+$('.no-click').on('click', function () {
+    $(this).removeClass('no-click').addClass('clicked');
+    $('.header-wrapper').css('display', 'flex');
+    $('body, html').addClass('no-scroll');
+});
+
+$('.clicked').on('click', function () {
+    $('.header-wrapper').css('display', 'none');
+    $('body, html').removeClass('no-scroll');
+    $(this).removeClass('clicked').addClass('no-click');
+});
+
+$("body").on("click", ".btn-scroll-top", function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, "slow")
+});
+
+//плавный скролл
+$(document).ready(function () {
+    $('.go_to').click(function (e) {
+        e.preventDefault();
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length !== 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 500);
+        }
+        return false;
+    });
+});
+//плавный скролл end
